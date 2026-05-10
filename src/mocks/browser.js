72fe8b7db68870225cae1a4d5d7bd99912fde987
DiggiLizz <lili.zapata@duocuc.cl>
2026-05-10 15:00:39 -0400

    Updates

--- src/mocks/browser.js
+++
@@ -1,6 +0,0 @@
-import { setupWorker } from 'msw/browser';
-import { handlers } from './handlers';
-
-// Service Worker que intercepta las peticiones en el navegador
-// Se inicia en main.jsx solo en modo development
-export const worker = setupWorker(...handlers);
